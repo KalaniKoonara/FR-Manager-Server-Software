9fcb62b27ee0c6bfa71258fffba743c96d69c369
pasan1992 <pnudawatta@gmail.com>
2015-05-10 17:20:44 +0530

    Functioning Ajax Example
    View : CheckGraphs
    Access the database through ajax request and output cpu usage through
    graphs.js asynchronously.

--- FRManager/App_Start/BundleConfig.cs
+++ FRManager/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace FRManager
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,6 +24,10 @@ namespace FRManager
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+             bundles.Add(new ScriptBundle("~/bundles/charts").Include("~/Scripts/Chart.js"));
+                      
+                      
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
